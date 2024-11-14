5c156de362564221b8fd8c1722e3ccdeeb3d9901
Bowen LIU <bowen.liu@ens.uvsq.fr>
2024-11-14 10:49:56 +0100

    [update][perf]:update rendering related functions
    	render.h
    	-calculateColor
    	1.Apply Lambertian scattering at the intersection point and calculate the returned color.
    	2.Recursive scattering: Recursively call calculateColor to simulate multiple scattering of light, and superimpose the attenuation value to achieve color attenuation.
    	3.Recursive depth control: depth controls the recursive depth to avoid infinite recursion.
    
    	-render
    	1.Monte Carlo Sampling:When rendering each pixel,generate random rays multiple times, add up their colors and take the average.
    	2.Gamma Correction: Before mapping the color to [0, 255], a square root operation is performed on each color component to perform gamma correction to make the image brightness more natural.
    
    	-createScene
    	1.Assign Lambertian materials to objects on the simple scene.
    
    	render.cpp
    	-Allow users to specify width, height, number of samples per pixel, and ppm file output path.

--- src/render.cpp
+++ src/render.cpp
@@ -1,10 +1,16 @@
 #include "render.h"
 
-int main()
+int main(int argc,char **argv)
 {
-    double width=200.0;
-    double height=100.0;
-    char *output_path="../output/output1.ppm";
+    if (argc!=5)
+    {
+        fprintf(stderr,"Usage:need 2 arguments\n");
+        exit(0);
+    }
+    double width=atof(argv[1]);
+    double height=atof(argv[2]);
+    int samples_per_pixel=atoi(argv[3]); //Each pixel is sampled argv[3] times
+    char *output_path=argv[4];
     //user-defined camera parameters
     Vec3 origin,lookat,v_up;
     double v_fov;
@@ -19,7 +25,7 @@ int main()
     //create a scene
     std::vector<Object*> scene=createScene();
     //render the image and output it to a file
-    render(width,height,scene,output_path,origin,lookat,v_up,v_fov);
+    render(width,height,scene,output_path,origin,lookat,v_up,v_fov,samples_per_pixel);
     std::cout<<"Render complete.Please check the outputfile: "<<output_path<<std::endl;
     return 0;
 }
--- src/render.h
+++ src/render.h
@@ -3,20 +3,25 @@
 #include "firstCollision.h"
 #include "camera.h"
 #include "../ppm_writer/ppm_writer.c"
+#include "lambertian.h"
 
 /*
     Calculates the color based on the intersection of the ray and the object. 
     If the ray intersects the object, the color is generated based on the normal vector of the intersection point; 
     otherwise, the background color is displayed.
 */
-Vec3 calculateColor(const Ray& ray, const std::vector<Object*>& scene)
+Vec3 calculateColor(const Ray& ray, const std::vector<Object*>& scene,int depth)
 {
     HitInfo hitInfo=findFirstCollision(ray,scene);
     if (hitInfo.hitObject)
     {
-        //Generate simple colors using normal vectors (map normal vectors to RGB values ​​in [0, 1])
-        Vec3 normal=(hitInfo.intersection-hitInfo.hitObject->getCenter()).normalize();
-        return (normal+Vec3(1.0, 1.0, 1.0))/2;
+        Ray scattered;
+        Vec3 attenuation;
+        if (depth<50&&hitInfo.hitObject->material->scatter(ray,hitInfo,attenuation,scattered))
+        {
+            return attenuation*calculateColor(scattered, scene, depth + 1);
+        }
+        return Vec3(0,0,0);//reach maximum recursion depth,return black
     }
     //Background color gradient
     Vec3 unitDirection=ray.direction.normalize();
@@ -26,7 +31,7 @@ Vec3 calculateColor(const Ray& ray, const std::vector<Object*>& scene)
 
 //rendering function
 void render(double width,double height,const std::vector<Object*>& scene,char* outputPath,const Vec3& origin,const Vec3& lookat,
-            const Vec3& v_up, double v_fov)
+            const Vec3& v_up, double v_fov,int samples_per_pixel)
 {
     double aspect=width/height;
     //use user-specified parameters so that the user can customize the camera viewing angle
@@ -37,14 +42,19 @@ void render(double width,double height,const std::vector<Object*>& scene,char* o
     {
         for (int j=0;j<width;++j)
         {
-            double u=double(j)/width;
-            double v=double(i)/height;
-            Ray ray=camera.getRay(u,v);
-            Vec3 color=calculateColor(ray,scene);
+            Vec3 color(0,0,0);
+            for (int s=0;s<samples_per_pixel;++s) {
+                double u=(j+randomFloat())/width;
+                double v=(i+randomFloat())/height;
+                Ray ray=camera.getRay(u, v);
+                color=color+calculateColor(ray,scene,0);
+            }
+            color=color/static_cast<double>(samples_per_pixel);//take the average of the samples
 
-            img[3*(i * static_cast<int>(width)+j)+0]=color.x;
-            img[3*(i * static_cast<int>(width)+j)+1]=color.y;
-            img[3*(i * static_cast<int>(width)+j)+2]=color.z;
+            //map color values ​​to the range [0,255]
+            img[3*(i*static_cast<int>(width)+j)+0]=sqrt(color.x);//Gamma Correction
+            img[3*(i*static_cast<int>(width)+j)+1]=sqrt(color.y);
+            img[3*(i*static_cast<int>(width)+j)+2]=sqrt(color.z);
         }
     }
     unsigned char* rgbImg =img2rgb(width,height,img);
@@ -56,9 +66,12 @@ void render(double width,double height,const std::vector<Object*>& scene,char* o
 //create scene
 std::vector<Object*> createScene()
 {
+    //assign Lambertian Materials
+    Lambertian* red_material=new Lambertian(Vec3(0.8,0.3,0.3));
+    Lambertian* ground_material=new Lambertian(Vec3(0.8,0.8,0.0));
     std::vector<Object*> scene;
-    scene.push_back(new Sphere(0.5,Vec3(0.0,0.0,-1.0)));
-    scene.push_back(new Sphere(100.0,Vec3(0.0,-100.5,-1.0)));//as ground
-    scene.push_back(new AABB(Vec3(-1.0,-0.5,-2.0),Vec3(1.0,0.5,-1.5)));
+    scene.push_back(new Sphere(red_material,0.5,Vec3(0.0,0.0,-1.0)));
+    scene.push_back(new Sphere(ground_material,100.0,Vec3(0.0,-100.5,-1.0)));//as ground
+    scene.push_back(new AABB(ground_material,Vec3(-1.0,-0.5,-2.0),Vec3(1.0,0.5,-1.5)));
     return scene;
 }

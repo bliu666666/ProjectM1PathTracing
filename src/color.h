7f5b9f7a93d309e124d41b574ab729382863e545
Abaul Dorian <abauldorian@gmail.com>
2024-11-28 10:13:16 +0100

    Implementation of a world (green) and antialiasing notion

--- src/color.h
+++ src/color.h
@@ -3,16 +3,31 @@
 
 #include "vec3.h"
 #include <iostream>
+#include <algorithm>
 
 using color = vec3;
 
-void write_color(std::ostream& out,const color& pixel_color){
-  int rbyte = static_cast<int>(255.999 * pixel_color.x);
-  int gbyte = static_cast<int>(255.999 * pixel_color.y);
-  int bbyte = static_cast<int>(255.999 * pixel_color.z);
-  
-  out << rbyte << ' ' << gbyte << ' ' << bbyte << std::endl;
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
 
+void write_color(std::ostream& out, const color& pixel_color, int samples_per_pixel) {
+    double r = pixel_color.x;
+    double g = pixel_color.y;
+    double b = pixel_color.z;
+
+    // Divide the color by the number of samples
+    double scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    // Write the translated [0,255] value of each color component
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
 #endif // !COLOR_H
--- src/ppm_writer.h
+++ src/ppm_writer.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <fstream>
+#include <random>
  
 #include "ray.h"
 #include "color.h"
@@ -15,26 +16,42 @@
 class PathTracer {
 public:
     int width, height;
+    int samples_per_pixel = 100;
     std::vector<Sphere> spheres;
 
     PathTracer(int w, int h) : width(w), height(h) {
-        spheres.emplace_back(vec3(0, 0, -5), 2, vec3(0.7, 0.3, 0.3)); // Red sphere
+        spheres.emplace_back(vec3(0, 0, -5), 2, vec3(1, 1, 0));
     }
-    void render(viewport &vp,float aspect_ratio,const char *path) {
+
+    double random_double() {
+        static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+        static std::mt19937 generator;
+        return distribution(generator);
+    }
+
+    void render(viewport &vp, float aspect_ratio, const char *path) {
         std::ofstream file(path);
         file << "P3\n" << width << " " << height << "\n255\n";
 
         for (int j = height - 1; j >= 0; --j) {
             for (int i = 0; i < width; ++i) {
-              // normalization depending on heigh image
-              float u = (2.0f * float(i) / float(width) - 1.0f) * aspect_ratio;
-              float v = 2.0f * float(j) / float(height) - 1.0f;
-              ray ray(vp.get_origin(), vec3(u, v, -1).normalize());  
-              vec3 color = Hitting::trace(ray, spheres);
-              int r = static_cast<int>(255.99 * color.x);
-              int g = static_cast<int>(255.99 * color.y);
-              int b = static_cast<int>(255.99 * color.z);
-              file << r << " " << g << " " << b << "\n";
+                vec3 pixel_color(0, 0, 0);
+                
+                for (int s = 0; s < samples_per_pixel; ++s) {
+                    float u = (2.0f * (float(i) + random_double()) / float(width) - 1.0f) * aspect_ratio;
+                    float v = 2.0f * (float(j) + random_double()) / float(height) - 1.0f;
+                    
+                    ray r(vp.get_origin(), vec3(u, v, -1).normalize());
+                    pixel_color = pixel_color + Hitting::trace(r, spheres);
+                }
+
+                pixel_color = pixel_color * (1.0 / samples_per_pixel);
+
+                int r = static_cast<int>(256 * std::clamp(pixel_color.x, 0.0, 0.999));
+                int g = static_cast<int>(256 * std::clamp(pixel_color.y, 0.0, 0.999));
+                int b = static_cast<int>(256 * std::clamp(pixel_color.z, 0.0, 0.999));
+                
+                file << r << " " << g << " " << b << "\n";
             }
         }
         file.close();

07b43643b594da4fbfc75aea91e8172f991e52a3
nathan <nat@debian.debnat.com>
2024-12-16 19:53:35 +0100

    Modifications des opérateurs entre vec3

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 
 
-vec3 viewport::pixelPosition(vec3 initialPosition, int i, int j, vec3 qx, vec3 qy){
+vec3 viewport::pixelPosition(int i, int j, vec3 qx, vec3 qy){
+    vec3 initialPosition=C-cam.b*(hx/2)-cam.v*(hy/2);
     return initialPosition+qx.operator*(i-1)+qy.operator*(j-1);
 }
--- src/viewport.h
+++ src/viewport.h
@@ -11,16 +11,16 @@ class viewport {
         int k; //width
         int m; //height
         camera cam;
-        vec3 C=cam.t.operator+(cam.E); //center
+        vec3 C=cam.t+cam.E; //center
         
 
         float hx=2*tan(theta/2);
         float hy=hx*((m-1)/(k-1));
-        vec3 qx=cam.b.operator*(hx/(k-1));
-        vec3 qy=cam.v.operator*(hy/(m-1));
+        vec3 qx=cam.b*(hx/(k-1));
+        vec3 qy=cam.v*(hy/(m-1));
 
-        vec3 initialPosition=C-cam.b.operator*(hx/2)-cam.v.operator*(hy/2);
-        vec3 pixelPosition(vec3 initialPosition, int i, int j, vec3 qx, vec3 qy);
+        
+        vec3 pixelPosition(int i, int j, vec3 qx, vec3 qy);
 
 
 }; 

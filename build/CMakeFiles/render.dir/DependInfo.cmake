f88d11db07100365bae301590d5060b0e6fec186
Abaul Dorian <abauldorian@gmail.com>
2025-04-07 15:59:11 +0200

    First dynamic parallelisation with OpenMP + better render
    - Parallelisation dynamic ( for dynamic ...)
    - Implementation direct light
    - Correction issues in BRDF implementation due to direct light implementation and duality between them and indirect light
    - Increase light source
    - Tone mapping ( ACES)
    - Reduice fireflies effect correction Reinhard mapping
    - Detection of emissive materials
    - Add new materials (glasses)
    - Ambiant occlusion
    - Integration of sky lighting

--- build/CMakeFiles/render.dir/DependInfo.cmake
+++ build/CMakeFiles/render.dir/DependInfo.cmake
@@ -8,19 +8,23 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/app/ppm_writer/ppm_writer.c" "CMakeFiles/render.dir/ppm_writer/ppm_writer.c.o" "gcc" "CMakeFiles/render.dir/ppm_writer/ppm_writer.c.o.d"
-  "/app/src/AABB.cpp" "CMakeFiles/render.dir/src/AABB.cpp.o" "gcc" "CMakeFiles/render.dir/src/AABB.cpp.o.d"
-  "/app/src/camera.cpp" "CMakeFiles/render.dir/src/camera.cpp.o" "gcc" "CMakeFiles/render.dir/src/camera.cpp.o.d"
-  "/app/src/lambertian.cpp" "CMakeFiles/render.dir/src/lambertian.cpp.o" "gcc" "CMakeFiles/render.dir/src/lambertian.cpp.o.d"
-  "/app/src/ray.cpp" "CMakeFiles/render.dir/src/ray.cpp.o" "gcc" "CMakeFiles/render.dir/src/ray.cpp.o.d"
-  "/app/src/raytracer.cpp" "CMakeFiles/render.dir/src/raytracer.cpp.o" "gcc" "CMakeFiles/render.dir/src/raytracer.cpp.o.d"
-  "/app/src/render.cpp" "CMakeFiles/render.dir/src/render.cpp.o" "gcc" "CMakeFiles/render.dir/src/render.cpp.o.d"
-  "/app/src/sphere.cpp" "CMakeFiles/render.dir/src/sphere.cpp.o" "gcc" "CMakeFiles/render.dir/src/sphere.cpp.o.d"
-  "/app/src/vec3.cpp" "CMakeFiles/render.dir/src/vec3.cpp.o" "gcc" "CMakeFiles/render.dir/src/vec3.cpp.o.d"
+  "/Users/dorian/ProjectM1PathTracing/ppm_writer/ppm_writer.c" "CMakeFiles/render.dir/ppm_writer/ppm_writer.c.o" "gcc" "CMakeFiles/render.dir/ppm_writer/ppm_writer.c.o.d"
+  "/Users/dorian/ProjectM1PathTracing/src/AABB.cpp" "CMakeFiles/render.dir/src/AABB.cpp.o" "gcc" "CMakeFiles/render.dir/src/AABB.cpp.o.d"
+  "/Users/dorian/ProjectM1PathTracing/src/camera.cpp" "CMakeFiles/render.dir/src/camera.cpp.o" "gcc" "CMakeFiles/render.dir/src/camera.cpp.o.d"
+  "/Users/dorian/ProjectM1PathTracing/src/lambertian.cpp" "CMakeFiles/render.dir/src/lambertian.cpp.o" "gcc" "CMakeFiles/render.dir/src/lambertian.cpp.o.d"
+  "/Users/dorian/ProjectM1PathTracing/src/ray.cpp" "CMakeFiles/render.dir/src/ray.cpp.o" "gcc" "CMakeFiles/render.dir/src/ray.cpp.o.d"
+  "/Users/dorian/ProjectM1PathTracing/src/raytracer.cpp" "CMakeFiles/render.dir/src/raytracer.cpp.o" "gcc" "CMakeFiles/render.dir/src/raytracer.cpp.o.d"
+  "/Users/dorian/ProjectM1PathTracing/src/render.cpp" "CMakeFiles/render.dir/src/render.cpp.o" "gcc" "CMakeFiles/render.dir/src/render.cpp.o.d"
+  "/Users/dorian/ProjectM1PathTracing/src/sphere.cpp" "CMakeFiles/render.dir/src/sphere.cpp.o" "gcc" "CMakeFiles/render.dir/src/sphere.cpp.o.d"
+  "/Users/dorian/ProjectM1PathTracing/src/vec3.cpp" "CMakeFiles/render.dir/src/vec3.cpp.o" "gcc" "CMakeFiles/render.dir/src/vec3.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.

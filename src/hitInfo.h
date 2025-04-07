be7663b5742b206d10f56f315a71a949fa131510
Bowen LIU <bowen.liu@ens.uvsq.fr>
2025-04-07 23:37:59 +0200

    add front_face field to HitInfo structure
    	This variable is used to determine whether the light is emitted from the air into the object or from the object to the air, thereby determining whether the refractive index is 1.0 / ref_idx or ref_idx.
    	At the same time, add a member function setFaceNormal to set the normal direction to ensure that it points to the camera side.This function will also be used in findFirstCollision() or in the logic of intersection with objects (such as Sphere, AABB).

--- src/hitInfo.h
+++ src/hitInfo.h
@@ -10,6 +10,13 @@ struct HitInfo
     Vec3 intersection;
     double distance;
     Vec3 normal;//normal vector
+    bool front_face;
 
     HitInfo():hitObject(nullptr),distance(DBL_MAX){}
+    // Set the normal direction to make sure it points to the camera side
+    void setFaceNormal(const Ray& ray,Vec3& outward_normal) 
+    {
+        front_face=ray.direction.dot(outward_normal)<0;
+        normal=front_face?outward_normal:-outward_normal;
+    }
 };

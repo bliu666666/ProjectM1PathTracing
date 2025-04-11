23b190a591e4e5d2bb13c28328aeae2483c34d57
Bowen LIU <bowen.liu@ens.uvsq.fr>
2025-04-11 23:40:23 +0200

    refactor:separate function declaration and implementation to prevent duplicate definitions during compilation

--- src/firstCollision.h
+++ src/firstCollision.h
@@ -6,22 +6,4 @@
 #include "AABB.h"
 #include "hitInfo.h"
 
-HitInfo findFirstCollision(const Ray& ray,const std::vector<Object*>& scene)
-{
-    HitInfo closestHit;
-    double tmin=DBL_MAX;//the initial minimum distance is set to infinity
-    for (auto* obj:scene)
-    {
-        Vec3 intersection;
-        double t;
-        Vec3 normal;
-        if (obj->intersect(ray,0.001f,tmin,intersection,t,normal))
-        {
-            closestHit.hitObject=obj;
-            closestHit.intersection=intersection;
-            closestHit.distance=t;
-            closestHit.setFaceNormal(ray, normal);
-        }
-    }
-    return closestHit;
-}
+HitInfo findFirstCollision(const Ray& ray,const std::vector<Object*>& scene);

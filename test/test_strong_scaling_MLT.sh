032ff7d397adf31ba17a6ad57476dcaf8c0705fa
bowen.liu <bowen.liu@ens.uvsq.fr>
2025-04-29 17:53:41 +0200

    A strong scaling test script for the rendering engine that integrates the MLT method

---
+++ test/test_strong_scaling_MLT.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+
+INPUT_FILE="input_strong_scaling.txt"
+OUTPUT="strong_scaling.txt"
+
+echo "Test strong scaling..." > $OUTPUT
+
+for t in 1 2 4 8 16
+do
+  export OMP_NUM_THREADS=$t
+  echo "Running with $t threads..." >> $OUTPUT
+  cat $INPUT_FILE | ../build/render 200 200 100 ../output/testMLT.ppm 3 1 >> $OUTPUT
+  echo "--------------------------------------------------------------"
+done
